12393160e62270935d8331aa163fbe446cd27368
Tu Phan <tuantu.phan@cimb.com>
2020-11-23 01:50:38 +0800

    Add some new files

---
+++ partials/content.php
@@ -0,0 +1,20 @@
+<div class="card mb-4">
+    <?php
+    $thumbnail_url = get_the_post_thumbnail(get_the_ID(), 'list_post_thumbnail', array(
+        'class' => 'card-img-top'
+    ));
+    if ($thumbnail_url === '') {
+        $thumbnail_url = '<img class="card-img-top size-list_post_thumbnail" src="http://placehold.it/750x300" alt="' . get_the_title() . '">';
+    }
+    echo $thumbnail_url;
+    ?>
+    <div class="card-body">
+        <a href="<?php the_permalink(); ?>">
+            <h2 class='card-title cus-exp-card-title'><?php the_title(); ?></h2>
+        </a>
+        <p class='card-text'><?php the_content(); ?></p>
+    </div>
+    <div class="card-footer text-muted">
+        Đăng lúc <?php post_relative_time(); ?> by <?php the_author() ?>
+    </div>
+</div>
--- single.php
+++ single.php
@@ -2,7 +2,89 @@
 
 get_header(); 
 ?>
-<h1>ABC Quynh</h1>
+<?php 
+    $cat_args = array(
+        'orderby' => 'name',
+        'order'   => 'ASC'
+    );
+    $categories = get_categories($cat_args);
+?>
+<div class="container cus-exp-header">
+    <div class="row">
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+            <h1 class="my-4"><?php echo get_bloginfo('name') ?>
+                <br/>
+                <small><?php echo get_bloginfo('description') ?></small>
+            </h1>
+
+            <!-- Loop Post -->
+            <?php
+                $post_id = get_the_ID();
+                $post_args = array(
+                    'p'                 => $post_id,
+                    'post_type'         => 'post',
+                    'posts_per_page'    => 10
+                ); 
+                $home_query = new WP_Query( $post_args );
+                if($home_query->have_posts()):
+            ?>
+            <?php
+                    while($home_query->have_posts()):
+                        $home_query->the_post();
+                        get_template_part( 'partials/content', get_post_format());
+                    endwhile;   
+                endif;
+            ?>
+		</div>
+		
+		<!-- Sidebar Widgets Column -->
+        <div class="col-md-4">
+
+            <!-- Search Widget -->
+            <div class="card my-4">
+                <h5 class="card-header text-white bg-dark">Tìm kiếm</h5>
+                <div class="card-body">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Tìm kiếm...">
+                        <span class="input-group-append">
+                            <button class="btn btn-primary" type="button">Go!</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Categories Widget -->
+            <div class="card my-4">
+                <h5 class="card-header text-white bg-dark">Thư mục</h5>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <ul class="list-unstyled mb-0">
+                                <?php
+                                    foreach($categories as $category) {
+                                        echo '<li><a href="' . get_category_link($category->term_id) . '">' . $category->name .' (' . $category->count .' bài viết )</a></li>';
+                                     }
+                                ?>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Side Widget -->
+            <div class="card my-4">
+                <h5 class="card-header">Side Widget</h5>
+                <div class="card-body">
+                    You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
 <?php
 get_footer();
 ?>
